030b64a0b9ec2de95e5dee7d34afd126ecfd2050
Yokoyama Hirokazu <hirokazu.yokoyama@showroom.co.jp>
2025-10-07 02:11:52 +0900

    feat: cache repository

--- repositories/cache_repository.go
+++ repositories/cache_repository.go
@@ -1,1 +1,41 @@
 package repositories
+
+import (
+	"encoding/json"
+	"github.com/SHOWROOM-inc/recursive_mock_gen/models"
+	"os"
+)
+
+type CacheRepository interface {
+	ReadCache(cacheFilePath string) (models.Cache, error)
+	WriteCache(cacheFilePath string, m *models.Cache) error
+}
+
+func NewCacheRepository() CacheRepository {
+	return &cacheRepository{}
+}
+
+type cacheRepository struct {
+}
+
+// ReadCache キャッシュファイルを読み込みます。もしキャッシュが無い場合は初期値を返します。
+func (r *cacheRepository) ReadCache(cacheFilePath string) (models.Cache, error) {
+	b, err := os.ReadFile(cacheFilePath)
+	if err != nil {
+		return models.Cache{}, nil
+	}
+
+	var m models.Cache
+	if err := json.Unmarshal(b, &m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func (r *cacheRepository) WriteCache(cacheFilePath string, m *models.Cache) error {
+	b, err := json.MarshalIndent(m, "", "  ")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(cacheFilePath, b, 0o644)
+}
